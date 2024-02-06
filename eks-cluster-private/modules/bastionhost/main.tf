3fa5e9582a1b9ba6c2b1d87a823ed4fc87f621ba
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-02-06 15:03:44 +0530

    Update main.tf

--- eks-cluster-private/modules/bastionhost/main.tf
+++ eks-cluster-private/modules/bastionhost/main.tf
@@ -18,7 +18,7 @@ data "aws_ami" "ubuntu" {
   }
 }
 data "template_file" "userdata" {
-  template = file("/../userdata/userdata.tpl")
+  template = file("/eks-cluster-private/userdata/userdata.tpl")
 
 }
 
