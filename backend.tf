33c8af0e7e2cb9647661c176c80cfec3f5f6c118
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-02-07 08:14:44 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
         bucket = "ar-bucket-143"
-       #key     = "mypro-24/terraform.tfstate"
+        key     = "mypro-24/terraform.tfstate"
         region = "ap-southeast-1"
   }
 }
