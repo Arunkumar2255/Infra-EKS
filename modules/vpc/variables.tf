4d903939483a2fb540525c96616a43cb0bd99d30
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-02-07 08:43:32 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,7 +1,7 @@
 ####### modules/vpc/variables.tf
 
 variable "vpc_cidr" {}
-variable "access_ip" {}
+#variable "access_ip" {}
 variable "public_sn_count" {}
 variable "public_cidrs" {
   type = list(any)
